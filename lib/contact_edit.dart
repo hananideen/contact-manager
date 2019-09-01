8d0498aa5927d1b341f53b5b053e702c4d3c54dc
Hanani <hananee92@gmail.com>
2019-09-01 21:18:54 +0800

    Edit contact add text controller

--- lib/contact_edit.dart
+++ lib/contact_edit.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 import './contact.dart';
 
-class EditContact extends StatelessWidget {
+class EditContact extends StatefulWidget {
 
   final Contact contact;
   EditContact({Key key, @required this.contact}) : super(key: key);
+
+  _EditContactState createState() => _EditContactState();
+}
+
+class _EditContactState extends State<EditContact> {
+
+  TextEditingController firstNameController = TextEditingController(text: "");
+  TextEditingController lastNameController = TextEditingController(text: "");
+  TextEditingController phoneNoController = TextEditingController(text: "");
+  TextEditingController emailController = TextEditingController(text: "");
+  TextEditingController dobController = TextEditingController(text: "");
+  TextEditingController genderController = TextEditingController(text: "");
+
+  @override
+  void dispose() {
+    firstNameController.dispose();
+    lastNameController.dispose();
+    phoneNoController.dispose();
+    emailController.dispose();
+    dobController.dispose();
+    genderController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,33 +43,71 @@ class EditContact extends StatelessWidget {
               radius: 40,
               child: Icon(Icons.person),
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Name'
+            Padding(
+              padding: const EdgeInsets.only(left:16.0, right: 16.0),
+              child: TextFormField(
+                controller: firstNameController = TextEditingController(text: widget.contact.firstName),
+                decoration: InputDecoration(
+                  labelText: 'First Name',
+                ),
               ),
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Phone Number'
+            Padding(
+              padding: const EdgeInsets.only(left:16.0, right: 16.0),
+              child: TextFormField(
+                controller: firstNameController = TextEditingController(text: widget.contact.lastName),
+                decoration: InputDecoration(
+                  labelText: 'Last Name',
+                ),
               ),
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Email'
+            Padding(
+              padding: const EdgeInsets.only(left:16.0, right: 16.0),
+              child: TextFormField(
+                controller: firstNameController = TextEditingController(text: widget.contact.phoneNo),
+                decoration: InputDecoration(
+                    labelText: 'Phone Number'
+                ),
               ),
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Date of Birth'
+            Padding(
+              padding: const EdgeInsets.only(left:16.0, right: 16.0),
+              child: TextFormField(
+                controller: firstNameController = TextEditingController(text: widget.contact.email),
+                decoration: InputDecoration(
+                    labelText: 'Email'
+                ),
               ),
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Gender'
+            Padding(
+              padding: const EdgeInsets.only(left:16.0, right: 16.0),
+              child: TextFormField(
+                controller: firstNameController = TextEditingController(text: widget.contact.dateOfBirth),
+                decoration: InputDecoration(
+                    labelText: 'Date of Birth'
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left:16.0, right: 16.0),
+              child: TextFormField(
+                controller: firstNameController = TextEditingController(text: widget.contact.gender),
+                decoration: InputDecoration(
+                    labelText: 'Gender'
+                ),
               ),
             ),
             RaisedButton(
-              padding: const EdgeInsets.all(8.0),
+              onPressed: () {
+                return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(firstNameController.text),
+                    );
+                  },
+                );
+              },
               textColor: Colors.white,
               color: Colors.blue,
               child: new Text("Save"),
