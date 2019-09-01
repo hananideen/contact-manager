019979be951b33cd249b5ce59461841e4ce7dd08
Hanani <hananee92@gmail.com>
2019-09-02 01:39:54 +0800

    Fix loading issue

--- lib/contact_list_view.dart
+++ lib/contact_list_view.dart
@@ -26,13 +26,25 @@ class _ContactListViewState extends State<ContactListView> {
     }
   }
 
+  deleteContact(id) async {
+    final String uri = 'https://mock-rest-api-server.herokuapp.com/api/v1/user/' +id;
+
+    var response = await http.delete(uri);
+    if (response.statusCode == 200) {
+//      String body = response.body;
+      print("success");
+    } else{
+      throw Exception('Failed');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder<List<Contact>>(
         future: fetchContact(),
         builder: (context, snapshot) {
-          if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
+          if (!snapshot.hasData || (snapshot.connectionState != ConnectionState.done)) return Center(child: CircularProgressIndicator());
 
           if(snapshot.connectionState == ConnectionState.done) {
             if(snapshot.hasError){
@@ -45,16 +57,45 @@ class _ContactListViewState extends State<ContactListView> {
                 final contact = snapshot.data[index];
 
                 return Dismissible(
-                  key: Key(contact.id),
+                  key: new ObjectKey(contact.id),
                   onDismissed: (direction) {
                     // Remove the item from the data source. Need to call API later
                     setState(() {
-                      snapshot.data.removeAt(index);
-                    });
+                      deleteContact(contact.id);
 
-                    // Then show a snackbar.
-                    Scaffold.of(context)
-                        .showSnackBar(SnackBar(content: Text('${contact.firstName} ${contact.lastName} deleted')));
+                      snapshot.data.removeAt(index);
+                      fetchContact();
+                      Scaffold.of(context)
+                          .showSnackBar(SnackBar(content: Text('${contact.firstName} ${contact.lastName} deleted')));
+
+//                      return showDialog(
+//                        context: context,
+//                        builder: (context) {
+//                          return AlertDialog(
+//                            content: Text("Are you sure you want to delete?"),
+//                            actions: <Widget>[
+//                              FlatButton(
+//                                child: const Text('NO'),
+//                                onPressed: () {
+//                                  Navigator.of(context).pop(ConfirmAction.CANCEL);
+//                                },
+//                              ),
+//                              FlatButton(
+//                                child: const Text('YES'),
+//                                onPressed: () {
+//                                  snapshot.data.removeAt(index);
+//
+//                                  Scaffold.of(context)
+//                                      .showSnackBar(SnackBar(content: Text('${contact.firstName} ${contact.lastName} deleted')));
+//
+//                                  Navigator.of(context).pop(ConfirmAction.ACCEPT);
+//                                },
+//                              )
+//                            ],
+//                          );
+//                        },
+//                      );
+                    });
                   },
 
                   background: Container(color: Colors.grey),
