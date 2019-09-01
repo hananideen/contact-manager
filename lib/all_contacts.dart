b9b7ce53af806de48d79796c88c55a7067c9f12c
Hanani <hananee92@gmail.com>
2019-09-01 16:46:19 +0800

    Avoid refresh tab when switching between tabs

--- lib/all_contacts.dart
+++ lib/all_contacts.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import './contact_list_view.dart';
 
-class AllContact extends StatelessWidget {
+class AllContact extends StatefulWidget {
+  _AllContact createState() => _AllContact();
+}
+
+class _AllContact extends State<AllContact> with AutomaticKeepAliveClientMixin<AllContact>{
+  @override
+  bool get wantKeepAlive => true;
+
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: ContactListView(),
